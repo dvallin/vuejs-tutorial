591022e87e3ce3c9262c2e64b76f9e0d1a8994c2
Maximilian Schuler <mschuler@itemis.com>
2017-11-30 12:55:07 +0100

    Static frontend and bootstrap support

---
+++ frontend/src/components/TodoListItem.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="todo-list-item">
+    <label>{{task.title}}</label>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'TodoListItem',
+    props: ['task']
+  }
+</script>
---
+++ frontend/test/components/TodoList.spec.ts
@@ -0,0 +1,29 @@
+import TodoList from '../../src/components/TodoList'
+import TodoListItem from '../../src/components/TodoListItem'
+import {Task} from "../../src/domain/Task";
+import {shallow} from 'vue-test-utils'
+
+const task = new Task("Some Title");
+
+describe('TodoList.vue', () => {
+  it('renders empty lists', () => {
+    const cmp = shallow(TodoList,  {
+      propsData: {
+        tasks: []
+      }
+    });
+    expect(cmp.element).toMatchSnapshot();
+    expect(cmp.vm.$el.childNodes.length).toBe(0);
+  });
+
+  it('renders three element lists', () => {
+    const cmp = shallow(TodoList,  {
+      propsData: {
+        tasks: [task,task,task]
+      }
+    });
+    expect(cmp.element).toMatchSnapshot();
+    expect(cmp.find(TodoListItem).vm.task.title).toEqual('Some Title');
+    expect(cmp.findAll(TodoListItem).length).toBe(3);
+  });
+});
